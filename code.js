a075aaefa583498273ab5cef23b2aca63f2910f7
aerick-321 <158105735+aerick-321@users.noreply.github.com>
2024-11-08 15:59:22 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,16 @@
 function tsp_hk(distance_matrix) {
+    // cities is the set of cities not visited so far, including start
+   function heldKarp(cities, start) {
+      if (cities == 2){
+        return length of tour that starts at start, goes directly to other city in cities
+      }
+      else{
+        return the minimum of
+          for each city in cities, unless the city is start {
+        // reduce the set of cities that are unvisited by one  (the old start), set the new start, add on the distance from old start to new start
+            heldKarp(cities - start, city) + distance from start to city
+          }
+      }
+   }
     return -1;
 }
